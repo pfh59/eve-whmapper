df23fbb505645d2a1564902ddce4928bed1174ae
PFH59 <pfhuillet@gmail.com>
2022-08-11 15:48:47 +0200

    Add  Db Test

--- WHMapper.Tests/Db/DbIntegrationTest.cs
+++ WHMapper.Tests/Db/DbIntegrationTest.cs
@@ -1,28 +1,73 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using WHMapper.Data;
+using WHMapper.Models.Db;
+using WHMapper.Repositories.WHMaps;
+using WHMapper.Tests.Attributes;
 
-namespace WHMapper.Tests;
+namespace WHMapper.Tests.Db;
 
-[Collection("Database")]
+[TestCaseOrderer("WHMapper.Tests.Orderers.PriorityOrderer", "WHMapper.Tests.Db")]
 public class DbIntegrationTest
 {
-    [Fact]
+    private WHMapperContext _context;
+
+    
+    public DbIntegrationTest()
+    {
+        //Create DB Context
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var optionBuilder = new DbContextOptionsBuilder<WHMapperContext>();
+        optionBuilder.UseNpgsql(configuration["ConnectionStrings:DefaultConnection"]);
+
+         _context = new WHMapperContext(optionBuilder.Options);
+    }
+
+
+
+    [Fact, TestPriority(100)]
     public async Task DeleteAndCreateDatabse()
     {
-            //Create DB Context
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .Build();
+        //Delete all to make a fresh Db
+        await _context.Database.EnsureDeletedAsync();
+        await _context.Database.EnsureCreatedAsync();
+    }
 
-            var optionBuilder = new DbContextOptionsBuilder<WHMapperContext>();
-            optionBuilder.UseNpgsql(configuration["ConnectionStrings:DefaultConnection"]);
+    [Fact, TestPriority(99)]
+    public async Task CRUD_WHMAP()
+    {
+        //Create IWHMapRepository
+        IWHMapRepository repoWHMAP = new WHMapRepository(_context);
 
-            var context = new WHMapperContext(optionBuilder.Options);
+        //ADD WHMAP
+        var result = await repoWHMAP.Create(new WHMap("FooBar"));
+        Assert.NotNull(result);
+        Assert.Equal("FooBar", result?.Name);
 
-            //Delete all to make a fresh Db
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
+        //GetALL
+        var results = (await repoWHMAP.GetAll())?.ToArray();
+        Assert.NotNull(results);
+        Assert.Single(results);
+        Assert.Equal("FooBar", results?[0].Name);
+
+        //GetByName
+        var result2 = await repoWHMAP.GetById(1);
+        Assert.NotNull(result2);
+        Assert.Equal("FooBar", result2?.Name);
+
+        //update
+        result2.Name = "FooBar Updated";
+        var result4 = await repoWHMAP.Update(result2.Id, result2);
+        Assert.NotNull(result2);
+        Assert.Equal("FooBar Updated", result2?.Name);
+
+        //Delete WHMAP
+        var result5 = await repoWHMAP.DeleteById(result2.Id);
+        Assert.NotNull(result5);
+        Assert.Equal("FooBar Updated", result5?.Name);
     }
 }
---
+++ WHMapper.Tests/Orderers/AlphabeticalOrderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace WHMapper.Tests.Orders
+{
+    public class AlphabeticalOrderer : ITestCaseOrderer
+    {
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(
+            IEnumerable<TTestCase> testCases) where TTestCase : ITestCase =>
+            testCases.OrderBy(testCase => testCase.TestMethod.Method.Name);
+    }
+}
+
