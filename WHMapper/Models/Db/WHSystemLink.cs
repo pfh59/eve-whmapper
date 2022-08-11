432d4844f736db5d58e54317f7fbb04b4625104f
PFH59 <pfhuillet@gmail.com>
2022-08-11 14:42:22 +0200

    fix

---
+++ WHMapper/Models/Db/WHSystemLink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WHMapper.Models.Db
+{
+    public class WHSystemLink
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        public int IdWHSystemFrom { get; set; }
+
+        [Required]
+        public int IdWHSystemTo  { get; set; }
+        
+     
+        public WHSystemLink(int idWHSystemFrom, int idWHSystemTo)
+        {
+            IdWHSystemFrom = idWHSystemFrom;
+            IdWHSystemTo = idWHSystemTo;
+        }
+    }
+}
+
