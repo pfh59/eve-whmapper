6ff9d62f4f86f68f7fd566a0971b2a77ce3ad657
pfh59 <pfhuillet@gmail.com>
2023-01-26 13:49:11 +0100

    Add IHM style Expand EveUniverseAPI to search more items Add systemLink Update Database

---
+++ WHMapper/Models/DTO/EveAPI/Universe/Group.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace WHMapper.Models.DTO.EveAPI.Universe
+{
+	public class Group
+	{
+
+        [JsonPropertyName("group_id")]
+        public int GroupId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("published")]
+        public bool Published { get; set; }
+
+        [JsonPropertyName("category_id")]
+        public int CategoryId { get; set; }
+
+        [JsonPropertyName("types")]
+        public int[] Types { get; set; }
+
+        [JsonConstructor]
+        public Group(int groupId, string name, bool published, int categoryId, int[] types) =>
+            (GroupId, Name, Published, CategoryId, Types) = (groupId, name, published, categoryId, types);
+    }
+}
+
