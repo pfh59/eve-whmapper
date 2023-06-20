f02807e725fe23dc90ef5db47455aa9696f276ab
pfhuillet <pfhuillet@gmail.com>
2023-06-20 22:35:58 +0200

    #93 #94 #95 #97
    Add Unit test
    Fix Unit test order and dependany
    Update packages

---
+++ WHMapper/Models/DTO/EveAPI/Search/SearchCoporationResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using WHMapper.Models.DTO.EveAPI.Universe;
+
+namespace WHMapper.Models.DTO.EveAPI.Search
+{
+	public class SearchAllainceResults
+	{
+        [JsonPropertyName("alliance")]
+        public int[]? Alliances { get; set; }
+    }
+}
+
