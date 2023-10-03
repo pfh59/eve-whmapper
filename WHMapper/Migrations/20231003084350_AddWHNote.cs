7d5a055691e0e6a9b1c92e7118009e488a230cba
pfhuillet <pfhuillet@gmail.com>
2023-10-03 18:08:59 +0200

    fix merging for feature #132

---
+++ WHMapper/Migrations/20231003084350_AddWHNote.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WHMapper.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWHNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SoloarSystemId = table.Column<int>(type: "integer", nullable: false),
+                    Comment = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_SoloarSystemId",
+                table: "Notes",
+                column: "SoloarSystemId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
---
+++ WHMapper/Repositories/WHNotes/IWHNoteRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using WHMapper.Models.Db;
+
+namespace WHMapper.Repositories.WHNotes
+{
+	public interface IWHNoteRepository : IDefaultRepository<WHNote, int>
+    {
+        public Task<WHNote?> GetBySolarSystemId(int solardSystemId);
+    }
+}
+
