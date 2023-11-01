fc3a0b6ec614e8fb6761bd13472ead905e646e50
pfh59 <109206285+pfh59@users.noreply.github.com>
2023-11-01 15:05:00 +0100

    Fix #164

--- src/WHMapper/Services/EveJwTExtensions/EveOnlineJwkDefaults.cs
+++ src/WHMapper/Services/EveJwTExtensions/EveOnlineJwkDefaults.cs
@@ -5,9 +5,11 @@ namespace WHMapper.Services.EveJwkExtensions
 	{
         public const string AuthenticationScheme = "EveOnlineBearer";
 
+
+        public static readonly string EVE_HOST = "login.eveonline.com" ;
         public static readonly string SSOUrl = "https://login.eveonline.com";
         public static readonly string JWKEndpoint = "https://login.eveonline.com/oauth/jwks";
-        public static readonly string ValideIssuer = "login.eveonline.com";
+        public static readonly string ValideIssuer = "https://login.eveonline.com";
         public static readonly string ValideAudience = "EVE Online";
     }
 }
