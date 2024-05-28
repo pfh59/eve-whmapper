997de7f0b7c62f1bce10366c3ddfb1e9b21937f0
pkoelemij <13510367+pkoelemij@users.noreply.github.com>
2024-05-28 19:56:56 +0200

    Fix formatting and remove unused usings

--- src/WHMapper/Services/Anoik/IAnoikServices.cs
+++ src/WHMapper/Services/Anoik/IAnoikServices.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using WHMapper.Models.DTO.Anoik;
+﻿using WHMapper.Models.DTO.Anoik;
 
 namespace WHMapper.Services.Anoik
 {
@@ -12,11 +8,7 @@ namespace WHMapper.Services.Anoik
         public string? GetSystemClass(string systemName);
         public string? GetSystemEffects(string systemName);
         public Task<IEnumerable<KeyValuePair<string, string>>> GetSystemStatics(string systemName);
-
         public IEnumerable<KeyValuePair<string, string>> GetSystemEffectsInfos(string effectName, string systemClass);
-
         public Task<IEnumerable<WormholeTypeInfo>> GetWormholeTypes();
-
-
     } 
 }
