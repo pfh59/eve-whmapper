48a3c3c7e287040bde870649215a4e78a6a96091
pkoelemij <13510367+pkoelemij@users.noreply.github.com>
2024-07-09 21:53:25 +0200

    Fix waiting in the EveMapperService

--- src/WHMapper/Services/EveMapper/EveMapperService.cs
+++ src/WHMapper/Services/EveMapper/EveMapperService.cs
@@ -1,8 +1,4 @@
-﻿using WHMapper.Models.DTO.EveAPI.Alliance;
-using WHMapper.Models.DTO.EveAPI.Character;
-using WHMapper.Models.DTO.EveAPI.Corporation;
-using WHMapper.Models.DTO.EveAPI.Universe;
-using WHMapper.Models.DTO.EveMapper.EveEntity;
+﻿using WHMapper.Models.DTO.EveMapper.EveEntity;
 using WHMapper.Services.EveAPI;
 
 namespace WHMapper.Services.EveMapper;
@@ -22,7 +18,7 @@ public class EveMapperService : IEveMapperService
 
     private async Task<TEntity?> Get<TEntity, TEveApiEntity>(
         int key,
-        Func<IEveAPIServices, TEveApiEntity> getEveApiEntityAction,
+        Func<IEveAPIServices, Task<TEveApiEntity?>> getEveApiEntityAction,
         Func<TEveApiEntity, TEntity> entityMap
     )
         where TEntity : AEveEntity
@@ -37,7 +33,7 @@ public class EveMapperService : IEveMapperService
             }
 
             // Get from api if cache is empty
-            var apiResult = getEveApiEntityAction.Invoke(_eveApiService);
+            var apiResult = await getEveApiEntityAction.Invoke(_eveApiService);
             if (apiResult == null)
             {
                 _logger.LogWarning($"{nameof(TEntity)} with Id {key} not found");
@@ -59,80 +55,80 @@ public class EveMapperService : IEveMapperService
 
     public async Task<AllianceEntity?> GetAlliance(int allianceId)
     {
-        return await Get<AllianceEntity, Alliance>(allianceId,
-            x => x.AllianceServices.GetAlliance(allianceId).Result,
+        return await Get(allianceId,
+            x => x.AllianceServices.GetAlliance(allianceId),
             x => new AllianceEntity(allianceId, x)
         );
     }
 
     public async Task<CharactereEntity?> GetCharacter(int characterId)
     {
-        return await Get<CharactereEntity, Character>(characterId,
-            x => x.CharacterServices.GetCharacter(characterId).Result,
+        return await Get(characterId,
+            x => x.CharacterServices.GetCharacter(characterId),
             x => new CharactereEntity(characterId, x)
         );
     }
 
     public async Task<CorporationEntity?> GetCorporation(int corporationId)
     {
-        return await Get<CorporationEntity, Corporation>(corporationId,
-            x => x.CorporationServices.GetCorporation(corporationId).Result,
+        return await Get(corporationId,
+            x => x.CorporationServices.GetCorporation(corporationId),
             x => new CorporationEntity(corporationId, x));
     }
 
     public async Task<ShipEntity?> GetShip(int shipTypeId)
     {
-        return await Get<ShipEntity, Models.DTO.EveAPI.Universe.Type>(shipTypeId,
-            x => x.UniverseServices.GetType(shipTypeId).Result,
+        return await Get(shipTypeId,
+            x => x.UniverseServices.GetType(shipTypeId),
             x => new ShipEntity(shipTypeId, x));
     }
 
     public async Task<SystemEntity?> GetSystem(int systemId)
     {
-        return await Get<SystemEntity, ESISolarSystem>(systemId,
-            x => x.UniverseServices.GetSystem(systemId).Result,
+        return await Get(systemId,
+            x => x.UniverseServices.GetSystem(systemId),
             x => new SystemEntity(systemId, x));
     }
 
     public async Task<ConstellationEntity?> GetConstellation(int constellationId)
     {
-        return await Get<ConstellationEntity, Constellation>(constellationId,
-            x => x.UniverseServices.GetConstellation(constellationId).Result,
+        return await Get(constellationId,
+            x => x.UniverseServices.GetConstellation(constellationId),
             x => new ConstellationEntity(constellationId, x));
     }
 
     public async Task<RegionEntity?> GetRegion(int regionId)
     {
-        return await Get<RegionEntity, Region>(regionId,
-            x => x.UniverseServices.GetRegion(regionId).Result,
+        return await Get(regionId,
+            x => x.UniverseServices.GetRegion(regionId),
             x => new RegionEntity(regionId, x));
     }
 
     public async Task<StargateEntity?> GetStargate(int stargateId)
     {
-        return await Get<StargateEntity, Stargate>(stargateId,
-            x => x.UniverseServices.GetStargate(stargateId).Result,
+        return await Get(stargateId,
+            x => x.UniverseServices.GetStargate(stargateId),
             x => new StargateEntity(stargateId, x));
     }
 
     public async Task<GroupEntity?> GetGroup(int groupId)
     {
-        return await Get<GroupEntity, Models.DTO.EveAPI.Universe.Group>(groupId,
-            x => x.UniverseServices.GetGroup(groupId).Result,
+        return await Get(groupId,
+            x => x.UniverseServices.GetGroup(groupId),
             x => new GroupEntity(groupId, x));
     }
 
     public async Task<WHEntity?> GetWormhole(int wormholeTypeId)
     {
-        return await Get<WHEntity, Models.DTO.EveAPI.Universe.Type>(wormholeTypeId,
-            x => x.UniverseServices.GetType(wormholeTypeId).Result,
+        return await Get(wormholeTypeId,
+            x => x.UniverseServices.GetType(wormholeTypeId),
             x => new WHEntity(wormholeTypeId, x));
     }
 
     public async Task<SunEntity?> GetSun(int sunTypeId)
     {
-        return await Get<SunEntity, Models.DTO.EveAPI.Universe.Type>(sunTypeId,
-            x => x.UniverseServices.GetType(sunTypeId).Result,
+        return await Get(sunTypeId,
+            x => x.UniverseServices.GetType(sunTypeId),
             x => new SunEntity(sunTypeId, x));
     }
 }
