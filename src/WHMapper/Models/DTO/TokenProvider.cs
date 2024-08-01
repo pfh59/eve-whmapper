f7c8a6e67d2f62f08c22f0bcf01d78b6ff7d9571
pfh59 <109206285+pfh59@users.noreply.github.com>
2024-08-01 18:23:56 +0200

    Multi fixes
    Fix : admin UI 
    FIx : menu UI
    Fix : antiforegy error
    Fix : route planner search system
    Intercept SDE checksum error when directory not present

--- src/WHMapper/Models/DTO/TokenProvider.cs
+++ src/WHMapper/Models/DTO/TokenProvider.cs
@@ -7,7 +7,6 @@ namespace WHMapper.Models.DTO
     {
         public string? AccessToken { get; set; }
         public string? RefreshToken { get; set; }
-        public string? XsrfToken { get; set; }
     }
 }
 
