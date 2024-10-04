6136a61475966dd0dfb5182e8514ee2ed0e0b9a5
pfh59 <109206285+pfh59@users.noreply.github.com>
2024-10-04 21:45:33 +0200

    Fix #306

--- src/WHMapper/Services/WHSignatures/IWHSignatureHelper.cs
+++ src/WHMapper/Services/WHSignatures/IWHSignatureHelper.cs
@@ -4,7 +4,7 @@ namespace WHMapper.Services.WHSignature
 {
     public interface IWHSignatureHelper
     {
-        const string SCAN_VALIDATION_REGEX = @"[A-Z]{3}-\d{3}\s+[\S\s]+?\s+\d*,\d+\s*\S*%\s+\d+[,.]*\d+\s*(UA|AU|km|m|а\.е\.|AE|км|м)";
+        const string SCAN_VALIDATION_REGEX = @"[A-Z]{3}-\d{3}\s+[\S\s]+?\s+\d*,\d+\s*\S*%\s+\d{1,3}(?:['\s]\d{3})*(?:,\d{1,2})?\s(?:UA|AU|km|m|а\.е\.|AE|км|м)";
         Task<bool> ValidateScanResult(string? scanResult);
         Task<IEnumerable<WHMapper.Models.Db.WHSignature>?> ParseScanResult(string scanUser, int currentSystemScannedId, string? scanResult);
         Task<bool> ImportScanResult(string scanUser, int currentSystemScannedId, string? scanResult, bool lazyDeleted);
